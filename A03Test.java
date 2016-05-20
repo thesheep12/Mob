44d93c1539b92e44f48ce68954c363c5e67cb5ef
thesheep12 <s.bielert@gmx.de>
2016-05-20 14:03:32 +0200

    Add files via upload

---
+++ A03Test.java
@@ -0,0 +1,28 @@
+/*                             Java für Anfänger               WS 2014/2015
+ * Übungsaufgabe 3                Testklasse                   A03Test.java
+ */
+
+
+public class A03Test {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Neue Kiste:");
+        
+        Behaeltnis kiste = new Behaeltnis("Flaschenkiste", 12, "Stück");
+        System.out.println(kiste);
+      /*  
+        System.out.println("Versuche, 4 Flaschen einzustellen.");
+        System.out.println("Es wurden " + kiste.einfuellen(4) + " Flaschen eingestellt.");
+        
+        System.out.println("Versuche, 20 Flaschen einzustellen.");
+        System.out.println("Es wurden " + kiste.einfuellen(20) + " Flaschen eingestellt.");
+        System.out.printf("Die Kiste enthält nun %d %s.\n", kiste.getFuellmenge(), kiste.einheit);
+        
+        System.out.println("Versuche, 10 Flaschen zu entnehmen.");
+        System.out.println("Es wurden " + kiste.entnehmen(10) + " Flaschen entnommen.");
+        
+        System.out.println("Versuche, 10 Flaschen zu entnehmen.");
+        System.out.println("Es wurden " + kiste.entnehmen(10) + " Flaschen entnommen.");*/
+    }
+}
